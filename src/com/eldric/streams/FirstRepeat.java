aae923f4d653b1023cc028c71bb6c7cbdb37b71b
Eldric Dsouza <eldric.dsouza@allianz.com>
2017-08-22 11:25:18 +0100

    testing double colon operator

--- src/com/eldric/streams/FirstRepeat.java
+++ src/com/eldric/streams/FirstRepeat.java
@@ -1,24 +1,35 @@
 package com.eldric.streams;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class FirstRepeat {
 
-        public static void main(String[] args) {
-            Map<Character, Long> collect =  "abcsdnvs".chars().mapToObj(i -> (char)i).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
-            collect.forEach( (x,y) -> System.out.println( "Key: " + x + " Val: " + y));
+    public static void main(String[] args) {
+        Map<Character, Long> collect =  "abcsdnvs".chars().mapToObj(i -> (char)i).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+        collect.forEach( (x,y) -> System.out.println( "Key: " + x + " Val: " + y));
 
-            Optional<Character> firstNonRepeat = collect.entrySet().stream().filter( (e) -> e.getValue() == 1).map(e -> e.getKey()).findFirst();
-            if(firstNonRepeat.isPresent()) {
-                System.out.println("First non repeating:" + firstNonRepeat.get());
-            }
-            Optional<Character> firstRepeat = collect.entrySet().stream().filter( (e) -> e.getValue() > 1).map(e -> e.getKey()).findFirst();
-            System.out.println("First repeating:" + firstRepeat.orElse(null));
+        collect.forEach(MyPrinter::print);
+        //Arrays.asList(1,2,3,4).forEach()
+
+        /*List<Integer> myArgs = Arrays.asList(1 ,2 ,3, 4);
+        myArgs.forEach(System.out::println); */
+
+        Optional<Character> firstNonRepeat = collect.entrySet().stream().filter( (e) -> e.getValue() == 1).map(e -> e.getKey()).findFirst();
+        if(firstNonRepeat.isPresent()) {
+            System.out.println("First non repeating:" + firstNonRepeat.get());
         }
+        Optional<Character> firstRepeat = collect.entrySet().stream().filter( (e) -> e.getValue() > 1).map(e -> e.getKey()).findFirst();
+        System.out.println("First repeating:" + firstRepeat.orElse(null));
     }
+}
+
+class MyPrinter {
+
+    public static void print(Character c, Long v) {
+        System.out.println("print");
+    }
+}
 
 
